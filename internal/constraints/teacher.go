de7f04db3ccbfc8fe79b67cbd5a6b55762d82e14
gaoyong <gaoyong06@qq.com>
2024-05-24 14:39:32 +0800

    修改: teacher约束

--- internal/constraints/teacher.go
+++ internal/constraints/teacher.go
@@ -28,15 +28,15 @@ type Teacher struct {
 	ID             int    `json:"id" mapstructure:"id"`                             // 自增ID
 	TeacherGroupID int    `json:"teacher_group_id" mapstructure:"teacher_group_id"` // 教师分组ID
 	TeacherID      int    `json:"teacher_id" mapstructure:"teacher_id"`             // 老师ID
-	TimeSlot       int    `json:"time_slot" mapstructure:"time_slot"`               // 时间段
+	TimeSlots      []int  `json:"time_slots" mapstructure:"time_slots"`             // 时间段
 	Limit          string `json:"limit" mapstructure:"limit"`                       // 限制: 固定排: fixed, 优先排: prefer, 禁排: not, 尽量不排: avoid
 	Desc           string `json:"desc" mapstructure:"desc"`                         // 描述
 }
 
 // 生成字符串
 func (t *Teacher) String() string {
-	return fmt.Sprintf("ID: %d, TeacherGroupID: %d, TeacherID: %d, TimeSlot: %d, Limit: %s, Desc: %s", t.ID,
-		t.TeacherGroupID, t.TeacherID, t.TimeSlot, t.Limit, t.Desc)
+	return fmt.Sprintf("ID: %d, TeacherGroupID: %d, TeacherID: %d, TimeSlots: %d, Limit: %s, Desc: %s", t.ID,
+		t.TeacherGroupID, t.TeacherID, t.TimeSlots, t.Limit, t.Desc)
 }
 
 // 获取班级固排禁排规则
@@ -73,13 +73,10 @@ func loadTeacherConstraintsFromDB() []*Teacher {
 // 生成规则校验方法
 func (t *Teacher) genConstraintFn(teachers []*models.Teacher) types.ConstraintFn {
 	return func(classMatrix *types.ClassMatrix, element types.Element, schedule *models.Schedule, taskAllocs []*models.TeachTaskAllocation) (bool, bool, error) {
+
 		teacherGroupID := t.TeacherGroupID
 		teacherID := t.TeacherID
-		timeSlot := t.TimeSlot
-
 		currTeacherID := element.GetTeacherID()
-		currTimeSlots := element.GetTimeSlots()
-
 		currTeacher, err := models.FindTeacherByID(currTeacherID, teachers)
 		if err != nil {
 			return false, false, err
@@ -88,15 +85,19 @@ func (t *Teacher) genConstraintFn(teachers []*models.Teacher) types.ConstraintFn
 		preCheckPassed := false
 		isReward := false
 
+		// 当前时间段,是否包含在约束时间段内
+		intersect := lo.Intersect(t.TimeSlots, element.TimeSlots)
+		isContain := len(intersect) > 0
+
 		// 固排,优先排是: 排了有奖励,不排有处罚
 		if t.Limit == "fixed" || t.Limit == "prefer" {
-			preCheckPassed = lo.Contains(currTimeSlots, timeSlot)
+			preCheckPassed = isContain
 			isReward = preCheckPassed && (teacherGroupID == 0 || lo.Contains(currTeacher.TeacherGroupIDs, teacherGroupID)) && (teacherID == 0 || teacherID == currTeacherID)
 		}
 
 		// 禁排,尽量不排是: 不排没关系, 排了就处罚
 		if t.Limit == "not" || t.Limit == "avoid" {
-			preCheckPassed = lo.Contains(currTimeSlots, timeSlot) && (teacherGroupID == 0 || lo.Contains(currTeacher.TeacherGroupIDs, teacherGroupID)) && (teacherID == 0 || teacherID == currTeacherID)
+			preCheckPassed = isContain && (teacherGroupID == 0 || lo.Contains(currTeacher.TeacherGroupIDs, teacherGroupID)) && (teacherID == 0 || teacherID == currTeacherID)
 			isReward = false
 		}
 		return preCheckPassed, isReward, nil
@@ -107,9 +108,9 @@ func (t *Teacher) genConstraintFn(teachers []*models.Teacher) types.ConstraintFn
 func (t *Teacher) getScore() int {
 	score := 0
 	if t.Limit == "fixed" {
-		score = 3
+		score = 6
 	} else if t.Limit == "prefer" {
-		score = 2
+		score = 4
 	}
 	return score
 }
@@ -118,9 +119,9 @@ func (t *Teacher) getScore() int {
 func (t *Teacher) getPenalty() int {
 	penalty := 0
 	if t.Limit == "not" {
-		penalty = 3
+		penalty = 6
 	} else if t.Limit == "avoid" {
-		penalty = 2
+		penalty = 4
 	}
 	return penalty
 }
