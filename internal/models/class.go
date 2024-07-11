4be9b723394136517aa14bd0f50f1e2ad5bca9e9
gaoyong <gaoyong79@gogoep.com>
2024-07-11 13:30:27 +0800

    修改：临时提交

--- internal/models/class.go
+++ internal/models/class.go
@@ -1,3 +1,11 @@
+/*
+ * @Author: gaoyong gaoyong79@gogoep.com
+ * @Date: 2024-07-11 12:00:57
+ * @LastEditors: gaoyong gaoyong79@gogoep.com
+ * @LastEditTime: 2024-07-11 13:30:12
+ * @FilePath: \course_scheduler\internal\models\class.go
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 // class.go
 package models
 
